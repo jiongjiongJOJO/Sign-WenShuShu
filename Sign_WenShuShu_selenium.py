a626acbc158c72a31274915d5a216537ce627671
囧囧JOJO <58727904+jiongjiongJOJO@users.noreply.github.com>
2021-01-27 12:57:37 +0800

    Update Sign_WenShuShu_selenium.py

--- Sign_WenShuShu_selenium.py
+++ Sign_WenShuShu_selenium.py
@@ -8,34 +8,32 @@ def send(sckey,text):
 user = os.environ.get('USER')
 password = os.environ.get('PASSWORD')
 SCKEY = os.environ.get('PUSH_MESSAGE')
-try:
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    b = webdriver.Chrome('chromedriver.exe', options=chrome_options)
 
-    b.get('https://www.wenshushu.cn/signin')
-    time.sleep(3)
-    #登录
-    b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[1]/ul/li[2]').click()
-    b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div[1]/div/div[1]/div[2]/input').send_keys(user)
-    b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div[1]/div/div[2]/div[2]/input').send_keys(password)
-    b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div[1]/div/button/span/span').click()
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+b = webdriver.Chrome('chromedriver.exe', options=chrome_options)
 
-    time.sleep(3)
-    b.refresh()
-    #点击签到键
-    time.sleep(1)
-    b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[1]/div[3]/div[2]/i').click()
-    time.sleep(1)
-    html=b.page_source
-    html = html.encode("utf8").decode("utf-8")
-    if not ('今日已打卡' in html or '打卡成功' in html):
-        send(SCKEY, '文叔叔签到失败')
-    print(html)
-except:
-    send(SCKEY, '文叔叔签到失败，未知错误')
+b.get('https://www.wenshushu.cn/signin')
+time.sleep(3)
+#登录
+b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[1]/ul/li[2]').click()
+b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div[1]/div/div[1]/div[2]/input').send_keys(user)
+b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div[1]/div/div[2]/div[2]/input').send_keys(password)
+b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div[1]/div/button/span/span').click()
+
+time.sleep(3)
+b.refresh()
+#点击签到键
+time.sleep(1)
+b.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[1]/div[3]/div[2]/i').click()
+time.sleep(1)
+html=b.page_source
+html = html.encode("utf8").decode("utf-8")
+if not ('今日已打卡' in html or '打卡成功' in html):
+    send(SCKEY, '文叔叔签到失败')
+print(html)
 
 
 
