eaac48de98e12f2a62a6bb510892cf2df6e33606
囧囧JOJO <58727904+jiongjiongJOJO@users.noreply.github.com>
2021-02-02 13:00:54 +0800

    Update Sign_WenShuShu_selenium.py

--- Sign_WenShuShu_selenium.py
+++ Sign_WenShuShu_selenium.py
@@ -8,9 +8,9 @@ import re
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 #message = {''}
-def send(push_token,text):
+def send(push_token,title,text):
     #http://pushplus.hxtrip.com/send?token=XXXXX&title=XXX&content=XXX&template=html
-    requests.get('http://pushplus.hxtrip.com/send?token='+push_token+'&title=文叔叔签到失败&content='+text+'&template=html')
+    requests.get('http://pushplus.hxtrip.com/send?token='+push_token+'&title='+title+'&content='+text+'&template=html')
 user = os.environ.get('USER')
 password = os.environ.get('PASSWORD')
 push_token = os.environ.get('PUSH_MESSAGE')
@@ -45,8 +45,9 @@ if ('今日已打卡' in html or '打卡成功' in html):
         result += names[i]+'：'+values[i]+'\n'
         logger.info('%s:%s' % (names[i].encode('utf8').decode('unicode_escape'),values[i].strip().encode('utf8').decode('unicode_escape')))
         #logger.info('%s:%s' % (names[i],values[i]))
-    send(push_token, result)
+    send(push_token,'文叔叔签到成功', result)
+    print(result)
 #print(html.encode(encoding='UTF-8',errors='strict').decode('UTF-8'))
 else:
-    send(push_token, html)
+    send(push_token,'文叔叔签到失败', html)
     logger.info(html.encode(encoding='UTF-8',errors='strict'))
