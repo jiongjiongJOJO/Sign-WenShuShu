121eeee2efbcd2218fcb99a3be96e8ff4eb54131
austin1000 <austin1000@foxmail.com>
2023-01-09 13:56:53 +0800

    加入对新人任务和广告的处理 (#9)
    * add click btn_close before daka
    
    used for account which quest wasn't be done
    
    * add multi account sign
    
     - split char is ;
    
    * add retry mechanism
    
     - max 5 retry times per account
    
    * fix output error in retry mechanism
    
    * 加入对广告的处理
    
    * Update Sign_WenShuShu_selenium.py
    
    1. 文叔叔的主要面向用户是中国人，已将log中的英文改成中文提示。修改的行数为：29，44，52，103，104
    2. 函数名不应该写成有歧义的单词，已修改为：`sign_wss`，修改的行数为：17，101
    
    修改比较匆忙，没有认真进行测试，希望测试后重新pr
    
    Co-authored-by: heshanu3d <33936333+heshanu3d@users.noreply.github.com>
    Co-authored-by: heshanu3d <heshann@foxmail.com>
    Co-authored-by: 囧囧JOJO <58727904+jiongjiongJOJO@users.noreply.github.com>

--- Sign_WenShuShu_selenium.py
+++ Sign_WenShuShu_selenium.py
@@ -5,74 +5,111 @@ import time
 
 import requests
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 
 def send(push_token, title, text):
     # http://www.pushplus.plus/send?token=XXXXX&title=XXX&content=XXX&template=html
     requests.get(f"http://www.pushplus.plus/send?token={push_token}&title={title}&content={text}&template=html")
 
+def sign_wss(user, password, token, msgs : list):
+    chrome_options = Options()
+    # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+    chrome_options.add_argument('--headless')
+    # 以最高权限运行
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    b = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
 
-user = os.environ.get('USER')
-password = os.environ.get('PASSWORD')
-push_token = os.environ.get('PUSH_MESSAGE')
+    b.get('https://www.wenshushu.cn/signin')
 
-if user is None:
-    exit()
-if password is None:
-    exit()
-if push_token is None:
-    push_token = ""
+    b.implicitly_wait(10)
+    logger.info("正在登陆...")
+    b.find_element(by=By.XPATH, value='//*[contains(text(),"密码登录")]').click()
+    time.sleep(1)
+    b.find_element(by=By.XPATH, value='//*[@placeholder="手机号 / 邮箱"]').send_keys(user)
+    time.sleep(1)
+    b.find_element(by=By.XPATH, value='//*[@placeholder="密码"]').send_keys(password)
+    time.sleep(1)
+    b.find_element(by=By.XPATH, value='//*[@type="submit"]').click()
+    time.sleep(1)
 
-chrome_options = Options()
-# 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
-chrome_options.add_argument('--headless')
-# 以最高权限运行
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-b = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+    b.implicitly_wait(10)
+    b.refresh()
+    time.sleep(1)
 
-b.get('https://www.wenshushu.cn/signin')
+    try:
+        logger.info("关闭广告和新手任务中...")
+        if b.find_element(by=By.CLASS_NAME, value="btn_close"):
+            b.find_element(by=By.CLASS_NAME, value="btn_close").click()
+        time.sleep(1)
+    except NoSuchElementException:
+        pass
 
-b.implicitly_wait(10)
-b.find_element(by=By.XPATH, value='//*[contains(text(),"密码")]').click()
-b.find_element(by=By.XPATH, value='//*[@placeholder="手机号 / 邮箱"]').send_keys(user)
-b.find_element(by=By.XPATH, value='//*[@placeholder="密码"]').send_keys(password)
-b.find_element(by=By.XPATH, value='//*[@type="submit"]').click()
-time.sleep(1)
+    b.implicitly_wait(10)
+    logger.info("{user} 正在打卡...".format(user=user))
+    b.find_element(by=By.CLASS_NAME, value="icondaka").click()
+    time.sleep(1)
 
-b.implicitly_wait(10)
-b.refresh()
-time.sleep(1)
+    b.implicitly_wait(10)
+    # 获取页面源码
+    html = b.page_source
 
-b.implicitly_wait(10)
-b.find_element(by=By.CLASS_NAME, value="icondaka").click()
-time.sleep(1)
+    if ('今日已打卡' in html or '打卡成功' in html):
+        html = html.replace('\n', '')
+        names = re.compile('class="m-title5">(.*?)</div>').findall(html)
+        values = re.compile('class="re-num m-text9">(.*?)</div>').findall(html)
+        result = ''
+        for i in range(len(names)):
+            if (names[i] == '手气不好'):
+                continue
+            result += names[i] + '：' + values[i] + '</br>'
+            logger.info('%s:%s' % (
+                names[i].encode('utf8').decode('unicode_escape'),
+                values[i].strip().encode('utf8').decode('unicode_escape')))
+        msg = (user + ',文叔叔签到成功,', result)
+    else:
+        msg = (user + ',文叔叔签到失败,', html)
+        logger.error(html.encode(encoding='UTF-8', errors='strict'))
+    msgs.append(msg)
 
-b.implicitly_wait(10)
-# 获取页面源码
-html = b.page_source
+    b.close()
 
-if ('今日已打卡' in html or '打卡成功' in html):
-    html = html.replace('\n', '')
-    names = re.compile('class="m-title5">(.*?)</div>').findall(html)
-    values = re.compile('class="re-num m-text9">(.*?)</div>').findall(html)
-    result = ''
-    for i in range(len(names)):
-        if (names[i] == '手气不好'):
-            continue
-        result += names[i] + '：' + values[i] + '</br>'
-        logger.info('%s:%s' % (
-            names[i].encode('utf8').decode('unicode_escape'),
-            values[i].strip().encode('utf8').decode('unicode_escape')))
-    send(push_token, '文叔叔签到成功', result)
-else:
-    send(push_token, '文叔叔签到失败', html)
-    logger.info(html.encode(encoding='UTF-8', errors='strict'))
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
 
-b.close()
+    users = os.environ.get('USER')
+    password = os.environ.get('PASSWORD')
+    push_token = os.environ.get('PUSH_MESSAGE')
+
+    if users is None:
+        exit()
+    if password is None:
+        exit()
+    if push_token is None:
+        push_token = ""
+    msgs = []
+
+    for user in users.split(';'):
+        retry = 0
+        while retry < 5:
+            success = True
+            try:
+                sign_wss(user, password, push_token, msgs)
+            except Exception as e:
+                logger.error("签到{user}账户时出现异常".format(user=user))
+                logger.info("已重试次数： " + str(retry + 1))
+                success = False
+            finally:
+                retry = retry + 1
+            if success:
+                break
+
+    push_text = ''
+    for msg in msgs:
+        push_text = push_text + msg[0] + msg[1]
+
+    send(push_token, '文叔叔签到结果', push_text)
