6093a256cedf25bbaca26381d11b6463daa0a559
囧囧JOJO <41473909@qq.com>
2023-11-18 19:00:36 +0800

    -feat: 增加反检测 更新requirements.txt

--- Sign_WenShuShu_selenium.py
+++ Sign_WenShuShu_selenium.py
@@ -1,20 +1,23 @@
-import logging
 import os
 import re
 import time
 import traceback
 import sys
+import ddddocr
+from io import BytesIO
+from PIL import Image
 import requests
+import html
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 def send(push_token, title, text):
     # http://www.pushplus.plus/send?token=XXXXX&title=XXX&content=XXX&template=html
-    requests.get(f"https://www.pushplus.plus/send?token={push_token}&title={title}&content={text}&template=html")
+    requests.get(f"https://www.pushplus.plus/send?token={push_token}&title={title}&content={text}&template=html", proxies=proxies)
 
 
 def hide_user(user):
@@ -26,6 +29,73 @@ def hide_user(user):
         return user[:2] + '*' * (at_index - 2) + user[at_index:]
     else:
         return user
+
+
+def captcha(element):
+    # 通过element获取image_url
+    page_source = element.parent.page_source
+    p = 'background-image: url\(&quot;(.*?)&quot;\);'
+    re_url_result = re.findall(p, page_source)
+
+    p2 = 'background-position: -(.*?)px -(.*?)px; background-size: (.*?)px (.*?)px; width: (.*?)px; height: (.*?)px; left: (.*?)px; top: (.*?)px;'
+    re_pix_result = re.findall(p2, page_source)
+
+    p3 = 'style="width: (.*?)px; height: (.*?)px; position:'
+    re_pix2_result = re.findall(p3, page_source)
+
+    background_image_url = ''
+    block_image_url = ''
+    for r in re_url_result:
+        temp_url = html.unescape(r)
+        if 'img_index=0' in temp_url:
+            block_image_url = temp_url
+        elif 'img_index=1' in temp_url:
+            background_image_url = temp_url
+
+    if len(re_pix_result) != 0:
+        if len(re_pix_result[0]) == 8:
+            pos_x, pos_y, pix_x, pix_y, width, height, left, top = map(int, map(float, re_pix_result[0]))
+        else:
+            raise Exception('获取验证码大小失败')
+    else:
+        raise Exception('获取验证码大小失败')
+
+    if len(re_pix2_result) != 0:
+        if len(re_pix2_result[0]) == 2:
+            pic_pix_x, pic_pix_y = map(int, map(float, re_pix2_result[0]))
+        else:
+            raise Exception('获取验证码大小失败')
+    else:
+        raise Exception('获取验证码大小失败')
+
+    if background_image_url == '' or block_image_url == '':
+        raise Exception('获取验证码链接失败')
+    # https://turing.captcha.qcloud.com/cap_union_new_getcapbysig?img_index=1&image=0279050000305c6000000015022ef4d62e47&sess=s08Ho0UBiQbe21Omc-WEu6Yhupg4ItJziklH_jULVBs5J3MNi39zTq_XAmpKkJk-v-rm9FCgKw_mVxvcub3EjC9Pbrzbd4DO4n1jRoKNH3FKHNMaUxDaD9a9tWO305ht7p_m2mLSUcdMXNxnOs7SqIZpfKgLPoZB1CnhrG8CCoMVxcyDTHINrk_SkJF5-ZZ6g_3MLB2idypmgbNgVst7lp-Uxk7k7jX5vYRwU3oV1tGDhTSZoTgDL48_qht3RQbys8qdEqVuxejEsCM6GY_3DeIp5lAx1Grv7UnAq9Fyhw4RnNse87-pvt25s_dO_YLzXfvUaY7QisDx24jVlCszq74DKOzJY2NK7JpLqR8VG0iCeIX1OXmYqIVETDWqxgkDKo
+    # 获取网络图片
+    # (response.content)
+    block_response = requests.get(block_image_url, proxies=proxies)
+    img_block = Image.open(BytesIO(block_response.content))
+    background_response = requests.get(background_image_url, proxies=proxies)
+    img_bg = Image.open(BytesIO(background_response.content))
+
+    # pos_x, pos_y, pix_x, pix_y, width, height, left, top
+    # (*70, *247, *345, *313, *60, *60, 25, 63)
+    img_block = img_block.resize((pix_x, pix_y))
+    img_block = img_block.crop((pos_x, pos_y, pos_x + width, pos_y + height))
+
+    img_bg = img_bg.resize((pic_pix_x, pic_pix_y))
+
+    img_block_bytes = BytesIO()
+    img_block.save(img_block_bytes, format='PNG')
+    img_block_bytes = img_block_bytes.getvalue()
+
+    img_bg_bytes = BytesIO()
+    img_bg.save(img_bg_bytes, format='PNG')
+    img_bg_bytes = img_bg_bytes.getvalue()
+
+    det = ddddocr.DdddOcr(det=False, ocr=False, show_ad=False)
+    res = det.slide_match(img_block_bytes, img_bg_bytes, simple_target=True)
+    return ((res['target'][2] + res['target'][0]) // 2) - left - 30
 
 
 def sign_wss(user, password, token, msgs : list, show_user_string : str):
@@ -56,30 +126,56 @@ def sign_wss(user, password, token, msgs : list, show_user_string : str):
     b.find_element(by=By.XPATH, value='//*[@type="submit"]').click()
     time.sleep(1)
 
-    b.implicitly_wait(10)
     b.refresh()
     time.sleep(1)
 
     try:
         print("关闭广告和新手任务中...")
-        if b.find_element(by=By.CLASS_NAME, value="btn_close"):
-            b.find_element(by=By.CLASS_NAME, value="btn_close").click()
+        b.find_element(by=By.CLASS_NAME, value="btn_close").click()
         time.sleep(1)
     except NoSuchElementException:
+        print("无广告或其他弹窗")
         pass
 
     b.implicitly_wait(10)
     print("{user} 正在打卡...".format(user=show_user_string))
     b.find_element(by=By.CLASS_NAME, value="icondaka").click()
-    time.sleep(1)
 
     b.implicitly_wait(10)
-    time.sleep(2)
-    
+    time.sleep(5)
+
     # 获取页面源码
     html = b.page_source
+    try:
+        if '今日已打卡' not in html or '打卡成功' in html:
+            # if b.find_element(by=By.CLASS_NAME, value='tc-fg-item'):
+            #     print('发现验证码', b.page_source)
+            # else:
+            #     print(b.page_source)
+            print('疑似存在验证码，正在处理验证码')
+            iframe = b.find_element(by=By.XPATH, value="//iframe[@id='tcaptcha_iframe_dy']")
+            b.switch_to.frame(iframe)
+            # 获取验证码滑块和背景图
+            element_block = b.find_element(by=By.CLASS_NAME, value='tc-fg-item')
+            # element_background_image = b.find_element(by=By.CLASS_NAME, value='tc-bg-img')
+            iv = captcha(element_block)
+            element_slider = b.find_element(by=By.CLASS_NAME, value='tc-slider-normal')
 
-    if ('今日已打卡' in html or '打卡成功' in html):
+            # 模拟鼠标点击并拖动滑块
+            action = ActionChains(b)
+            action.click_and_hold(element_slider).move_by_offset(iv, 0).release().perform()
+            print('验证码处理完成')
+
+    except:
+        # print(b.page_source)
+        print(traceback.format_exc())
+
+    time.sleep(5)
+    b.implicitly_wait(10)
+    b.switch_to.default_content()
+    html = b.page_source
+
+    if '今日已打卡' in html or '打卡成功' in html:
         html = html.replace('\n', '')
         names = re.compile('class="m-title5">(.*?)</div>').findall(html)
         values = re.compile('class="re-num m-text9">(.*?)</div>').findall(html)
@@ -99,6 +195,11 @@ def sign_wss(user, password, token, msgs : list, show_user_string : str):
 
 if __name__ == '__main__':
     sys.stdout.reconfigure(encoding='UTF-8')
+
+    proxies = {
+        'http': None,
+        'https': None
+    }
 
     users = os.environ.get('USER')
     password = os.environ.get('PASSWORD')
